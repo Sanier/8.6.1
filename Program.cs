ea0b7b6e557a88eac96c43be755cd248f00a2bf5
Sanier <Sanier20@mail.ru>
2021-10-09 17:49:52 +0300

    Проверка на удаление при превышении времени последнего затрагивания файла

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _8._6._1
 {
@@ -6,7 +7,31 @@ namespace _8._6._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Пропишите путь до файла как в примере: C:/Users/user/Desktop/example.bin");
+            string filePath = Console.ReadLine();
+
+            if (!File.Exists(filePath))
+            {
+                try
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(filePath);
+                    var lastMod = File.GetLastAccessTime(filePath);
+                    if (lastMod <= DateTime.Now - TimeSpan.FromMinutes(30))
+                    {
+                        dirInfo.Delete(true);
+                        Console.WriteLine("Delete");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Время последнего использования файлов не превысило 30 минут");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка {0}", ex.Message);
+                }
+            }
         }
     }
 }
